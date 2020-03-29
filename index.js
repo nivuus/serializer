6295ed675183f1822fc81d08b4aa80af5f75f1b0
Maxime Allanic <maxime@allanic.me>
2020-03-29 18:43:07 +0200

    [FIX] lot of bugs

--- index.js
+++ index.js
@@ -5,74 +5,158 @@
  * Created by Maxime Allanic <maxime@allanic.me> at 25/03/2020
  */
 
-function serialize (object) {
+const defaultConstructors = {
+    'File': {
+        parse: async (value) => {
+            return value;
+        },
+        format: async (value) => {
+            return await value.arrayBuffer();
+        }
+    },
+    'FileList': {
+        parse: async (value) => {
+            return value;
+        },
+        format: async (value) => {
+            return Array.prototype.slice.call(value);
+        }
+    }
+};
+
+function findConstructor(object, constructors) {
+    var key;
+    if (constructors)
+        key = Object.keys(constructors).find((key) => {
+            return (!constructors[ key ].check && key === object.constructor.name)
+                || (constructors[ key ].check && constructors[ key ].check(object));
+        });
+
+    if (key)
+        return constructors[ key ];
+    else {
+        var key = Object.keys(defaultConstructors).find((key) => {
+            return key === object.constructor.name && (!defaultConstructors[ key ].check || defaultConstructors[ key ].check(object));
+        });
+        return defaultConstructors[ key ];
+    }
+}
+
+async function serializeObject(object, setPrototype, config, serialized) {
+    var values = {};
+    for (const key in object) {
+        values[ key ] = await serialize(object[key], config, serialized);
+    }
+
+    await Promise.all(Object.getOwnPropertyNames(object).map(async (key) => {
+        if (values.hasOwnProperty(key))
+                return;
+        values[ key ] = await serialize(object[ key ], config, serialized)
+    }));
+
+    if (!setPrototype)
+        await Promise.all(Object.getOwnPropertyNames(Object.getPrototypeOf(object)).map(async (key) => {
+            if (values.hasOwnProperty(key))
+                return;
+            values[ key ] = await serialize(object[ key ], config, serialized);
+            values[ key ].prototype = true;
+        }));
+    return values;
+}
+
+async function serialize(object, config, serialized) {
     var values = {};
 
-    if (typeof object === 'undefined') {
+    // Manager circular reference
+    if (!serialized) {
+        if (typeof object === 'object')
+            serialized = [
+                object
+            ];
+        else
+            serialized = [];
+    }
+    else if (typeof object === 'object' && serialized.includes(object))
+        return {
+            type: 'mirror'
+        };
+    else if (typeof object === 'object')
+        serialized.push(object);
+
+    // Manage nil values
+    if (typeof object === 'undefined')
         return {
             type: 'undefined',
             value: undefined
         };
-    }
+    else if (object === null)
+        return {
+            type: 'null',
+            value: null
+        };
 
-    if (typeof object === 'object') {
-        var isClassGlobal;
-        eval(`isClassGlobal = typeof ${ object.constructor.name } === 'function'`);
-        for (const key in object) {
-            values[ key ] = serialize(object[key]);
-        }
+    var constructor = findConstructor(object, config ? config.constructors : null);
 
-        Object.getOwnPropertyNames(object).forEach((key) => {
-
-            values[ key ] = {
-                type: typeof object[ key ],
-                value: object[ key ]
-            }
+    var constructorName = object.constructor.name;
+    if (constructor)
+        return await constructor.serialize(object, async (value, setPrototype) => {
+            return serializeObject(value, setPrototype, config, serialized);
         });
+    // Manage all other object values expect ArrayBuffer
+    else if (typeof object === 'object' && object.constructor.name !== 'ArrayBuffer') {
 
-        if (!isClassGlobal)
-            Object.getOwnPropertyNames(Object.getPrototypeOf(object)).forEach((key) => {
-                if (values.hasOwnProperty(key))
-                    return;
-                values[ key ] = {
-                    type: typeof object[ key ],
-                    prototype: true,
-                    value: object[ key ]
-                }
-            });
+        var isClassGlobal;
+        eval(`isClassGlobal = typeof ${ constructorName } === 'function'`);
+        values = await serializeObject(object, isClassGlobal, config, serialized);
     }
+    // Or if is Number, Boolean just insert value
     else
         values = object;
 
     return {
         type: typeof object,
-        constructor: object.constructor.name,
+        constructorName: object.constructor.name,
         value: values
     };
 };
 
-function unserialize(object, onMethodCall, parentKey) {
-    var value;
-
-    if (object.type === 'object') {
-        var isClassExist;
-        eval(`isClassExist = typeof ${ object.constructor } === 'function'`);
-
-        if (isClassExist)
-            eval(`value = new ${ object.constructor}()`);
-        else
-            eval(`class ${ object.constructor } {}; value = new ${ object.constructor }`);
-        for (const key in object.value) {
-            if (!isClassExist && object.value[key].prototype && object.value[key].type === 'function')
-                value.__proto__[ key ] = unserialize(object.value[key], onMethodCall, parentKey ? (parentKey + '.') : '' + key);
-            else if (!object.value[key].prototype || (object.value[key].type !== 'function'))
-                value[ key ] = unserialize(object.value[key], onMethodCall, parentKey ? (parentKey + '.') : '' + key);
+async function unserializeObject(values, setPrototype, destination, config, parentKey) {
+    for (const key in values) {
+        if (setPrototype && values[ key ].prototype && values[ key ].type === 'function')
+            destination.__proto__[ key ] = await unserialize(values[ key ], parentKey ? (parentKey + '.') : '' + key);
+        else if (!values[ key ].prototype || (values[ key ].type !== 'function')) {
+            destination[ key ] = await unserialize(values[ key ], config, parentKey ? (parentKey + '.') : '' + key);
         }
     }
-    else if (object.type === 'function') {
-        return function (...args) {
-            return onMethodCall(parentKey, ...args);
-        };
+}
+
+async function unserialize(object, config, parentKey) {
+    var value;
+    if (!object)
+        return null;
+
+    if (config && config.constructors && config.constructors[ object.constructorName ]) {
+        var unserializedValue = {};
+        await unserializeObject(object.value, config.setPrototype, unserializedValue, config, parentKey);
+        return await config.constructors[ object.constructorName ].unserialize(unserializedValue, object, config, parentKey);
+    }
+    else if (defaultConstructors[ object.constructorName ]) {
+        var unserializedValue = {};
+        await unserializeObject(object.value, config.setPrototype, unserializedValue, config, parentKey);
+        return await config.defaultConstructors[ object.constructorName ].unserialize(unserializedValue, object, config, parentKey);
+    }
+    else if (object.type === 'object' && object.constructorName !== 'ArrayBuffer') {
+        var isClassExist;
+        eval(`isClassExist = typeof ${ object.constructorName } === 'function'`);
+
+        if (isClassExist) {
+            eval(`value = new ${ object.constructorName }()`);
+            await unserializeObject(object.value, true, value, config, parentKey);
+        }
+        else {
+            eval(`class ${ object.constructorName } {}; value = new ${ object.constructorName }`);
+            await unserializeObject(object.value, true, value, config, parentKey);
+        }
     }
     else
         value = object.value;
--- test/index.js
+++ test/index.js
@@ -9,56 +9,126 @@ const $serializer = require('../');
 const $assert = require('chai').assert;
 
 describe('Serializer', () => {
-    it('should serialize simple object', () => {
+    it('should serialize simple object', async () => {
         var o = {
             o: true
         };
-        var t = $serializer.serialize(o);
+        var t = await $serializer.serialize(o);
 
-        $assert.deepEqual(o, $serializer.unserialize(t));
+        $assert.deepEqual(o, await $serializer.unserialize(t));
     });
 
-    it('should serialize string', () => {
+    it('should serialize string', async () => {
         var o = 'test';
-        var t = $serializer.serialize(o);
+        var t = await $serializer.serialize(o);
 
-        $assert.equal(o, $serializer.unserialize(t));
+        $assert.equal(o, await $serializer.unserialize(t));
     });
 
-    it('should serialize complete class', () => {
+    it('should serialize complete class', async () => {
         class Test {
             constructor (r) {
                 this.r = r;
             }
         }
         var o = new Test('ok');
-        var t = $serializer.serialize(o);
-        var u = $serializer.unserialize(t);
+        var t = await $serializer.serialize(o);
+        var u = await $serializer.unserialize(t);
 
         $assert.deepEqual(o, u);
     });
 
-    it('should serialize existing class', () => {
+    it('should serialize existing class', async () => {
         var o = new Error('ok');
-        var t = $serializer.serialize(o);
-        var u = $serializer.unserialize(t);
+        var t = await $serializer.serialize(o);
+        var u = await $serializer.unserialize(t);
 
         $assert.ownInclude(o, u);
     });
 
-    it('should call onCall when method is called', () => {
+    it('should serialize custom class', async () => {
+        class Test {
+            constructor (r) {
+                this.r = r;
+            }
+
+            test() {
+
+            }
+        }
+
+        var config = {
+            constructors: {
+                Test: {
+                    serialize: async function (value, serializeObject) {
+
+                        return {
+                            type: 'object',
+                            constructorName: 'Test',
+                            value: await serializeObject(value)
+                        };
+                    },
+                    unserialize: async function (object) {
+                        return new Test(object.r)
+                    }
+                }
+            }
+        }
+
+        var o = new Test('ok');
+        var t = await $serializer.serialize(o, config);
+        var u = await $serializer.unserialize(t, config);
+        $assert.ok(u.constructor.name === 'Test');
+        $assert.ownInclude(o, u);
+    });
+
+    it('should serialize function', async () => {
+        var config = {
+            constructors: {
+                'Function': {
+                    serialize: async function () {
+                        var callerId = 'call:test';
+                        var result = {
+                            type: 'function',
+                            constructorName: 'Function',
+                            callerId: callerId
+                        };
+                        return result;
+                    },
+                    unserialize: async (unseralized, response, config, parentKey) => {
+                        var f;
+                        eval(`f = function ${ parentKey }() {return 'called';}`);
+                        return f;
+                    }
+                }
+            }
+        }
+
+        var o = {
+            o: function () {
+
+            }
+        };
+        var t = await $serializer.serialize(o, config);
+        var u = await $serializer.unserialize(t, config);
+        var result = u.o();
+        $assert.ok(result === 'called');
+//        $assert.ownInclude(o, u);
+    })
+
+    it('should call onCall when method is called', async () => {
         var o = function () {
 
         };
-        var t = $serializer.serialize(o);
-        var u = $serializer.unserialize(t, (key, ...args) => {
+        var t = await $serializer.serialize(o);
+        var u = await $serializer.unserialize(t, (key, ...args) => {
             $assert.equal(args[ 0 ], 'true');
         });
 
         u('true');
     });
 
-    it('should call onCall when class method is called', () => {
+    it('should call onCall when class method is called', async () => {
         class Test {
             constructor (r) {
                 this.r = r;
@@ -69,9 +139,20 @@ describe('Serializer', () => {
             }
         }
         var o = new Test('ok');
-        var t = $serializer.serialize(o);
-        var u = $serializer.unserialize(t, (key, ...args) => {
-            $assert.equal(args[ 0 ], 'true');
+        var t = await $serializer.serialize(o);
+
+        var u = await $serializer.unserialize(t, {
+            setPrototype: true,
+            constructors: {
+                'Function': {
+                    unserialize: () => {
+                        return function (...args) {
+                            $assert.equal(args[ 0 ], 'true');
+                            done();
+                        }
+                    }
+                }
+            }
         });
 
         u.test('true');
